75d0a20173046b58ceab119191b5399c8288b01e
Aarti <103598054+aartisutar@users.noreply.github.com>
2022-08-15 14:16:10 +0530

    Add files via upload

---
+++ C math Function.c
@@ -0,0 +1,12 @@
+    #include<stdio.h>
+    #include<math.h>
+                int main()
+    {
+            printf("%f\n",pow(4,3));
+            printf("%f\n",sqrt(100));
+            printf("%f\n",ceil(5.25));
+            printf("%f\n",floor(5.25));
+            getch();
+
+            return 0;
+    }
