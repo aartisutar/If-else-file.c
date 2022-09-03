d779cfd2a5fa4fb1018b0fe2e848d12cb917c4fb
Aarti <103598054+aartisutar@users.noreply.github.com>
2022-09-03 09:29:55 +0530

    Add files via upload

---
+++ Year 2005.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+
+{
+int A,M;
+
+printf("enter the current year:");
+
+scanf("%d",&A);
+
+printf("enter the age :");
+
+scanf("%d",&M);
+
+printf("The date of year the person is: %d",A-M);
+
+getch();
+
+}
